414910a46986eadec1b53fd98ed321c97da4b920
Ghanashyam Aryal <anjanaryal13@gmail.com>
2024-08-28 16:48:47 +0545

    middleware

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 } from "cloudinary"
+import fs from "fs"
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadFileCloudinary = async (localpath) => {
+    try {
+        if (!localpath) return null
+
+        const response = await v2.uploader.upload(localpath, {
+            resource_type: "auto"
+        })
+        fs.unlinkSync(localpath)
+        return response
+
+    } catch (error) {
+        fs.unlinkSync(localpath)
+        return null
+
+
+    }
+
+}
